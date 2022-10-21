c3ac98b462e21b0d1814de85732cce472ba83d60
Nico Wagner <info@ni-wa.de>
2022-10-21 07:24:32 +0200

    moved to express-utils (index.ts)

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,42 @@ import {
   App,
   Config,
   dbConfig,
-  DbConfigInterface,
+  getMongoConnectionString,
+  IDbConfig,
   logger,
 } from '@kopf02/express-utils';
 //Convict config
-export type CustomConvictConfig = DbConfigInterface;
+export type CustomConvictConfig = IDbConfig;
 new Config(dbConfig('mongodb', 'rest-api'));
 
 import MaintenanceMiddleware from './middlewares/maintenance.middleware';
 import indexRoute from './routes/index.route';
 import * as mongoose from 'mongoose';
-import { mongoDBURL, mongoOptions } from './database/mongodb';
 import { Mongoose } from 'mongoose';
 import mailTemplatesRoute from './routes/mailTemplates.route';
+import MailServerRoute from './routes/mailServer.route';
 
 logger.level = 'silly';
-const app = new App(new indexRoute(), new mailTemplatesRoute());
+const app = new App(
+  new indexRoute(),
+  new mailTemplatesRoute(),
+  new MailServerRoute()
+);
 let dataSource: Mongoose;
 
 if (process.env.NODE_ENV !== 'test') {
   logger.info('Trying to connect to database...');
-  mongoose.connect(mongoDBURL, mongoOptions).then((res) => {
-    //AppDataSource.initialize().then((_r) => {
-    app.afterMiddlewares(new MaintenanceMiddleware(false).ExpressMiddleWare);
-    app.init();
-    app.getServer().set('etag', false);
-    app.listen();
-    dataSource = res;
-    //console.log('app.listen();');
-  });
+  mongoose
+    .connect(getMongoConnectionString(), { authSource: 'admin' })
+    .then((res) => {
+      //AppDataSource.initialize().then((_r) => {
+      app.afterMiddlewares(new MaintenanceMiddleware(false).ExpressMiddleWare);
+      app.init();
+      app.getServer().set('etag', false);
+      app.listen();
+      dataSource = res;
+      //console.log('app.listen();');
+    });
   //});
 } else {
   //app.afterMiddlewares(new MaintenanceMiddleware().ExpressMiddleWare);
